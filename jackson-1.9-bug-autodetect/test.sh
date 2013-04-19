76a43a503d382a8a54b413cd1b7902e7f85d8d61
Chris Winters <chris@cwinters.com>
2013-04-19 13:19:05 -0400

    jackson shell test run fix

--- jackson-1.9-bug-autodetect/test.sh
+++ jackson-1.9-bug-autodetect/test.sh
@@ -1,10 +1,14 @@
 echo "Testing against Jackson 1.8.5..."
 rm *.class
-javac -cp 'lib-1.8/*' *.java 
-java -cp .:'lib-1.8/*' org.junit.runner.JUnitCore FieldBugTest
+echo "  OK 1.8: cleaned classes"
+javac -cp 'lib/*':'lib/1.8/*' *.java 
+echo "  OK 1.8: compiled"
+java -cp .:'lib/*':'lib/1.8/*' org.junit.runner.JUnitCore FieldBugTest
 
 echo "Testing against Jackson 1.9.0..."
 rm *.class
-javac -cp 'lib-1.9/*' *.java 
-java -cp .:'lib-1.9/*' org.junit.runner.JUnitCore FieldBugTest
+echo "  OK 1.9: cleaned classes"
+javac -cp 'lib/*':'lib/1.9/*' *.java 
+echo "  OK 1.9: compiled"
+java -cp .:'lib/*':'lib/1.9/*' org.junit.runner.JUnitCore FieldBugTest
 
