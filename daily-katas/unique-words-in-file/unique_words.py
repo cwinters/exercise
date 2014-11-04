17ba180d31c7ce40944167912423cc79c5db9a11
Chris Winters <chris@cwinters.com>
2014-11-04 16:49:39 -0500

    Add a python version

---
+++ daily-katas/unique-words-in-file/unique_words.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import operator, re, sys
+
+line_splitter = re.compile('(\s+|\-\-)')
+empty = re.compile('^\s*$')
+digits = re.compile('^\d+$')
+
+def clean(word):
+    if empty.match(word) or digits.match(word):
+        return None
+    return word.lower()
+
+def count_words(counts, path):
+    for line in file(path):
+        for word in line_splitter.split(line.strip()):
+            cleaned = clean(word)
+            if not cleaned:
+                continue
+            elif cleaned in counts:
+                counts[cleaned] += 1
+            else:
+                counts[cleaned] = 1
+
+if __name__ == "__main__":
+    print ' '.join(sys.argv[1:])
+    words = {}
+    for path in sys.argv[1:]:
+        count_words(words, path)
+    for word, count in sorted(words.items(), key=operator.itemgetter(1), reverse=True):
+        print "%s = %s" % (word, count)
